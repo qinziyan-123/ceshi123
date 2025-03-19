773aa61c9368174447b9eb6dc91dc642223cd23e
qinziyan <qinziyan@tangsec.com>
2025-03-19 11:40:05 +0800

    初始化

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+module.exports = function (eleventyConfig) {
+  // 复制静态资源（CSS、JS）到输出目录
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
+
+  return {
+    // markdownTemplateEngine: "njk", // 如果需要使用 Nunjucks 作为 Markdown 模板引擎，可以取消注释
+    templateFormats: ["html", "njk", "md"], // 支持的模板格式
+    pathPrefix: "/QiyuanWarehouse/", // 设置路径前缀
+    dir: {
+      input: "src",    // 输入文件夹
+      output: "dist",  // 输出文件夹
+      includes: "includes", // 模板文件存放目录
+    },
+  };
+};
---
+++ src/posts/post1.md
@@ -0,0 +1,8 @@
+---
+title: My First Post
+date: 2023-10-01
+---
+
+# My First Post
+
+This is my first post written in **Markdown**.
